fec45cfba6f767f95e92f02bdc07152055da9170
virginiah894 <virgyperry@gmail.com>
2019-11-16 13:48:18 +0300

    add configuration settings for the application

---
+++ config.py
@@ -0,0 +1,16 @@
+import class Config:
+class Config:
+  NEWS_API_BASE_URL =
+  NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+  SECRET_KEY = os.environ.get('SECRET_KEY')
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
