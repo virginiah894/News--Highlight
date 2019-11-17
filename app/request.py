8fd89ce2fc4e5ba3f658e67a828b73410121d284
virginiah894 <virgyperry@gmail.com>
2019-11-17 16:03:23 +0300

    add request method to get results from API

--- app/request.py
+++ app/request.py
@@ -1,3 +1,28 @@
 from app import app
+import urllib.request,json
+from . models import news
+
+News = news.News
 # fetching the api key
 api_key = app.config['NEWS_API_KEY']
+
+# base url
+base_url = app.config["NEWS_API_BASE_URL"]
+def get_news(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_news_url = base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        get_news_response = json.loads(get_news_data)
+
+        news_results = None
+
+        if get_news_response['results']:
+            news_results_list = get_news_response['results']
+            news_results = process_results(movie_results_list)
+
+
+    return news_results
