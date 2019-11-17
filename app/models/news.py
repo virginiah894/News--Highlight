c3c85d27018487b254daaa317cd7c3f9360b5041
virginiah894 <virgyperry@gmail.com>
2019-11-17 14:13:11 +0300

    add a news.py file which  has a class instance of the news

---
+++ app/models/news.py
@@ -0,0 +1,13 @@
+class News:
+    '''
+    Movie class to instantiate news
+    '''
+
+    def __init__(self,id,title,overview,poster,more info,vote_count):
+        self.id =id
+        self.title = title
+        self.overview = overview
+        self.poster = 'https://image.tmdb.org/t/p/w500/'+ poster
+        self.vote_average = vote_average
+        self.vote_count = vote_count
+
